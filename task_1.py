8f2469a65ef24fd1aec204da15e37b4796e479e2
ChatanyaGupta <Chatanyagupta3@gmail.com>
2026-08-20 12:28:17 +0530

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,27 @@
+# To perform addition and subtraction
+
+'''a = int(input("enter the first number"))
+b = int(input("Enter second number: "))  
+print("Sum of two numbers:", a + b)
+print("Subtraction of two numbers:",a - b)
+print("Multiplication of two:", a * b)
+print("Division of two:", a / b)'''
+
+# to find remainder when a number is divided by z
+
+
+'''number = int(input("Enter the number: "))
+
+z = int(input("Enter the number to divide by: "))
+
+remainder = number % z
+
+print("The remainder is:", remainder)'''
+
+
+# Check the type of variable assigned using input() function 
+
+
+k = input("Enter something (a number, a word, anything!): ")
+
+print("The type of the variable is:", type(k))

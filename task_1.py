4430f30e3a3bac9c132e4aef44f0c7e0f8991d90
ChatanyaGupta <Chatanyagupta3@gmail.com>
2026-08-25 11:55:03 +0530

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -22,6 +22,15 @@ print("The remainder is:", remainder)'''
 # Check the type of variable assigned using input() function 
 
 
-k = input("Enter something (a number, a word, anything!): ")
+'''k = input("Enter something (a number, a word, anything!): ")
 
-print("The type of the variable is:", type(k))
+print("The type of the variable is:", type(k))'''
+
+# temperature in celsius from the user and convert it into Faherneit 
+
+
+temp = int(input("Enter the Temperature in Celsius: "))
+
+temp1 = ((9/5)*temp) + 32
+
+print("Temperature in Fahrenheit: ", temp1)

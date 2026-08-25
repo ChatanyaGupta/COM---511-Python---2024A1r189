925a228ed853f85653686648ae6c133023ae975d
ChatanyaGupta <Chatanyagupta3@gmail.com>
2026-08-25 12:35:59 +0530

    Add files via upload
    This work is done on 25/08/2026

---
+++ task_2.py
@@ -0,0 +1,41 @@
+# write a python program to take total minutes as inpput and convert it into hours and remaining minutes
+
+'''minutes = int(input("Enter total minutes: "))
+
+
+hours = minutes // 60
+minutes = minutes % 60
+
+print("Hours =", hours)
+print("Remaining minutes =", minutes)'''
+
+
+# to take two digut number as input and print sum of its digits 
+
+'''x = int(input("Enter a two digit number"))
+
+first_digit = x // 10 # "//" yh isliye lgaya kyunki isse float value nhi aati 
+second_digit = x % 10
+
+sum = first_digit + second_digit
+
+print("Sum of digits =", sum)'''
+
+# to take an amount in rupees and acalculate how many 500 and 100 niotes are there 
+
+'''amount = int(input("Enter amount in rupees: "))
+
+
+notes_500 = amount // 500
+remaining = amount % 500
+
+notes_100 = remaining // 100
+remaining = amount% 10
+
+print("500 notes =", notes_500)
+print("100 notes =", notes_100)'''
+
+
+# to take two inputs a and b, swap their values using a temporary variable and print updated value 
+
+a =

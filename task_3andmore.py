9453c2b0eff1ffaf509bbd903f053c0ad2be3d01
ChatanyaGupta <Chatanyagupta3@gmail.com>
2026-09-01 12:49:36 +0530

    Add files via upload

---
+++ task_3andmore.py
@@ -0,0 +1,22 @@
+# to detect double space in a string 
+
+'''text = input("Enter a string: ")
+
+print(text.find("  "))'''
+
+
+# to replace double spaces from problem 3 with single spaces
+
+'''text = input("Enter a string: ")
+
+text = text.replace("  ", " ")
+
+print(text)'''
+
+
+
+# to format the following letter using escape sequence characters 
+
+letter = "Dear Sir,\n\tI am writing this letter to request your permission.\nThank you.\nYours sincerely,\nChatanya"
+
+print(letter)

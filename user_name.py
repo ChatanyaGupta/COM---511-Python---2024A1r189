fe6b3edfce826340256ac82adde13ff8ebaa777f
ChatanyaGupta <Chatanyagupta3@gmail.com>
2026-09-01 12:32:03 +0530

    Add files via upload

---
+++ user_name.py
@@ -0,0 +1,11 @@
+#wap to display a user centred name followed by "Good afternoon" using input()  function (using f-string)
+
+name = input("Enter your name: ")
+print(f"Good Afternoon, {name}")
+
+# to detect double space in a string 
+
+# to replace double spaces from problem 3 with single spaces
+# to format the following letter using escape sequence characters 
+
+

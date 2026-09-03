cc29d2b8bc752cee0dc78c00f8dc041f1cd3cc63
ChatanyaGupta <Chatanyagupta3@gmail.com>
2026-09-03 12:02:40 +0530

    Add files via upload

---
+++ task 4.py
@@ -0,0 +1,14 @@
+# check whether email is endig with @ or not 
+email = input("Enter email address: ")
+
+print(email.endswith("mietjammu.in"))
+
+
+
+mobile = input("Enter mobile number: ")
+
+masked = "******"+mobile[-4:]
+
+print("Masked mobile number =", masked)
+
+
